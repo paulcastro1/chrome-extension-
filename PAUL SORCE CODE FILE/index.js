344f8cee3ced46a844fb574fd3769578fea95294
fullstackpaul <81339523+paulcastro1@users.noreply.github.com>
2021-10-20 10:30:54 -0500

    Update index.js

--- PAUL SORCE CODE FILE/index.js
+++ PAUL SORCE CODE FILE/index.js
@@ -2,25 +2,29 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+console.log(leadsFromLocalStorage)
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
- inputEl.value = ""
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     renderLeads()
 })
 
-// 1. Wrap the code below in a renderLeads() function
 function renderLeads() {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) { 
-        listItems += 
-      `    <li>
-      <a target='_blank' href='${myLeads[i]}'>
-          ${myLeads[i]}
-      </a>
-  </li>
-`
-
-    console.log(listItems)    }
-    ulEl.innerHTML = listItems  
-}
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems  }
